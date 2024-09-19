4fbebe24efc1e417000bf12b3d9b78b8077ff112
ecomnazar <ecomnazar.me@gmail.com>
2024-09-20 00:27:55 +0500

    [deploy to prod]

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,8 +1,7 @@
 export const GREENS_MINIAPP_URL =
-  "http://t.me/Greens_test_bot/Greens_test?startapp=walletConnected=true";
+  "https://t.me/GreensfiBot/play?startapp=walletConnected=true";
 // export const GREENS_MINIAPP_URL = "http://t.me/GreensfiBot/play";
-export const GREENS_MINIAPP_EMPTY_URL =
-  "http://t.me/Greens_test_bot/Greens_test";
+export const GREENS_MINIAPP_EMPTY_URL = "http://t.me/GreensfiBot/play";
 
 export const SUCCESS_PAGE_URL =
   "https://wallet.greensfi.com/greens/pay/success";
--- src/contexts/JsonRpcContext.tsx
+++ src/contexts/JsonRpcContext.tsx
@@ -1600,14 +1600,6 @@ export function JsonRpcContextProvider({
           },
         });
 
-        const transactionToSign = {
-          txID: realTransaction.transaction.txID,
-          raw_data: realTransaction.transaction.raw_data,
-          raw_data_hex: realTransaction.transaction.raw_data_hex,
-        };
-
-        console.log(transactionToSign);
-
         const { result } = await client!.request<{ result: any }>({
           chainId,
           topic: session!.topic,
@@ -1615,15 +1607,15 @@ export function JsonRpcContextProvider({
             method: "tron_signTransaction",
             params: {
               address,
-              transaction: transactionToSign,
+              transaction: {
+                ...realTransaction,
+              },
             },
           },
         });
 
-        console.log(result);
-
         const signature = result.signature; // Подпись транзакции
-        const rawTransaction = { ...transactionToSign, signature };
+        const rawTransaction = { ...realTransaction.transaction, signature };
 
         async function sendSignedTransaction(transaction: any) {
           try {
